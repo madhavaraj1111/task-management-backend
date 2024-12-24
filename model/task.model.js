3a8546a39a563b771edc5548c0c37ec9d7060f7c
Madhavaraj C <madhavaraj1111@gmail.com>
2024-12-24 15:34:41 +0530

    Date logic change in model

--- model/task.model.js
+++ model/task.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const taskSchema = mongoose.Schema({
   taskName: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, required: true },
+  createdAt: { type: Date, default: () => Date.now(), required: true },
   finishingTime: { type: Date, default: Date.now, required: true },
   checked: { type: Boolean, required: true },
 });
